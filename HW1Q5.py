b78c89f6ea743ed2d0ed3bc77a9901df64478b4d
Ted <theodore.morley@mail.mcgill.ca>
2017-11-04 15:34:19 -0400

    Added distance comparison

--- HW1Q5.py
+++ HW1Q5.py
@@ -15,6 +15,8 @@
 #Desired output: A graph showing the average distance of the population to the target structure vs the generation.
 ##For each generation, write this data to an output file
 import random
+import sys
+sys.path.append("/usr/local/lib/python2.7/site-packages")
 import RNA
 
 #Note: Need to make sure that this is what he means by sampled uniformly
@@ -39,22 +41,34 @@ def initializePopulation(struct_size, pop_size):
 def allStructMFE(pop):
     mapping = dict()
     for seq in pop:
-        mapping[seq] = ##
+        mapping[seq] = RNA.fold(seq)[0]
     return True
 
-#Returns a numerical value indicating the fitness of the MFE struct mfe, in rgards to the target structure
+#Returns a numerical value indicating the fitness of the MFE struct mfe, in regards to the target structure
 #Uses the base pair distance in RNAdistance
-def calcFitness(mfe, target_struct)
-    return True
+def calcFitness(mfe, target_struct):
+    return RNA.bp_distance(mfe, target_struct)
 
 #Writes the average distance of the population to the target structure, tagged at this generation, to out
 def plot(fitness_dict, generation, out):
     return True
 
+#Defines reproduction rate as a function of base pair distance
+def R(d):
+    #
+
 #Returns a new population list, where each sequence is replicated with the fitness in the dict
 #Additionaly, each nucleotide has a mutation rate of mut_rate
-def replicate(fitness_dict, mut_rate):
-    return True
+#Essentially the workflow is as follows:
+#For N times, we select a sequence with probabilities defined by R
+##This sequence is replicated with error rate mut_rate, and the result is added to the new population
+def replicate(fitness_dict, mut_rate, N):
+    #Sample with replacement from the sequences, N times, with weights equal to the reproductive fitness
+    ##As defined by R
+    newPop = random.choices(fitness_dict.keys(), weights=[R(x) for x in fitness_dict.itervalues()], k=N)
+    for i in range(N):
+        #
+    return newPop
 
 def reactor(T, L, N, G, M, output_file):
     population = initializePopulation(L, N)
@@ -66,5 +80,5 @@ def reactor(T, L, N, G, M, output_file):
         #fitnesses is a dict mapping from an seq to the fitness of its mfe structure
         #In relation to the target structure provided
         plot(fitnesses, i, output_file)
-        population = replicate(fitnesses, M)
+        population = replicate(fitnesses, M, N)
     
